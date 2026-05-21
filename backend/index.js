7dac691ddcb95a8c1ae55bc6ebc513bd51c923f9
Peter Zemturis <pzemturis@gmail.com>
2026-05-21 15:10:51 +0200

    addded pg

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,25 @@ const express = require('express'),
   path = require('path'),
   cors = require('cors');
 
+const dotenv = require('dotenv'),
+  { Client } = require('pg');
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI
+});
+
+app.get('/api/cities', async (_request, response) => {
+  const { rows } = await client.query('SELECT * FROM cities WHERE name = $1', [
+    'Stockholm'
+  ]);
+
+  response.send(rows);
+});
+
+client.connect();
+
 const app = express();
 app.use(cors());
 app.get('/api', (_request, response) => {
